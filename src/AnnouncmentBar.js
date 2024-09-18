881ea9ba590a1501ce51e1413e7999cee92ea9d6
Vishal <vishal@technersolutions.com>
2024-09-18 12:26:39 +0530

    announcment-added

---
+++ src/AnnouncmentBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AnnouncmentBar() {
+  return (
+    <div>
+      <div className="annbar bg-danger text-white py-3">
+        Hii
+      </div>
+    </div>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AnnouncmentBar from './AnnouncmentBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit with Vishal<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AnnouncmentBar></AnnouncmentBar>
     </div>
   );
 }
